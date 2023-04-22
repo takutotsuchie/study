6194c0016e806e8615015521504125a9c8eeb383
takutotsuchie <t.tsuchie@buysell-technologies.com>
2023-04-22 17:47:06 +0900

    helpを追加

--- message.go
+++ message.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func help() {
 	fmt.Println("help")
+	fmt.Println("		start: 記録を開始")
+	fmt.Println("		end: 記録を終了")
+	fmt.Println("		ls: 歴代の月の記録を表示")
+	fmt.Println("		today: 今日の勉強時間を表示")
 }
